409f221117a49c343326e8f990a426b8e3ecfb54
jeevan reddy <jeevanece759@gmail.com>
2021-05-06 09:26:34 +0530

    update

--- frontend/js/color.js
+++ frontend/js/color.js
@@ -4,12 +4,13 @@ console.log("hello");
  var x=document.getElementById("a").value;
  var y=document.getElementById("b").value;
  var z=document.getElementById("c").value;
+ 
  console.log(x,y,z);
  x=parseInt(x);
  y=parseInt(y);
  z=parseInt(z);
  console.log(typeof x, typeof y, typeof z);
-
+ document.getElementById("rgb").innerHTML="rgb(x,y,z)";
 document.getElementById("rgb").style.backgroundColor="rgb(0,0,255)";
 function on(){
     console.log("changed");
