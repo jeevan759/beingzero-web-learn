7ddeb9bf0f79d745129739de3db54b5bbee28a20
jeevan reddy <jeevanece759@gmail.com>
2021-05-07 12:18:36 +0530

    push

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,4 +1,5 @@
-function.submit(){
-  let   x=document.getElementById("email").value;
-  document.getElementById("login").innerHTML=<a href="/html/login">login</a>;
+function submit(){
+  let x=document.getElementById("email").value;
+  console.log("submitted");
+  document.getElementById("button").innerHTML= "<a href="/html/login.html" >submit</a>";
 }
